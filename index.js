51a53f30148212d9f7b267db9dc5b61b0980f59f
Kajal <kajal@selegic.com>
2021-12-18 23:07:34 +0530

    File read/write

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+// Sync File read
+let txtIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(txtIn);
+
+// Sync File update
+const txtOut = `Hello World\nupdated on ${Date.now()}`;
+fs.writeFileSync('./txt/input.txt',txtOut,'utf-8');
+
+// Async File read
+fs.readFile('./txt/input.txt', 'utf-8', (error, data)=>{
+    console.log(data);
+})
+console.log('Read File : ');
